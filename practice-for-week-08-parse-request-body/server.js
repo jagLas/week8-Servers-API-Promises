d471263f7cfc2c0bf44f40589599f7fc7b47f284
jagLas <jglasher@gmail.com>
2023-02-16 15:46:20 -0500

    finished phase 1

--- practice-for-week-08-parse-request-body/server.js
+++ practice-for-week-08-parse-request-body/server.js
@@ -6,6 +6,14 @@ let server;
 /******************* DO NOT CHANGE THE CODE ABOVE THIS LINE *******************/
 
 // Your code here
+const http = require('http');
+
+server = http.createServer((res, req) => {
+
+});
+
+server.listen(5000, () => console.log('Successfully started the server on port 5000'))
+
 
 /******************************************************************************/
 /******************* DO NOT CHANGE THE CODE BELOW THIS LINE *******************/
