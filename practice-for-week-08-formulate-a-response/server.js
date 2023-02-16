c145bf444002d6310ea54a0dd255ea726de828c6
jagLas <jglasher@gmail.com>
2023-02-16 14:48:19 -0500

    practice done

--- practice-for-week-08-formulate-a-response/server.js
+++ practice-for-week-08-formulate-a-response/server.js
@@ -1,1 +1,27 @@
 // Your code here
+const http = require('http');
+
+const port = 5000;
+
+const responseBody = `
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Hello World!</title>
+</head>
+<body>
+  <h1>Hello there!</h1>
+</body>
+</html>
+`;
+
+const server = http.createServer((req, res) => {
+    res.statusCode = 200;
+    res.setHeader('content-type', 'text/html');
+    res.write(responseBody);
+    res.end();
+});
+
+server.listen(port, () => console.log('Server listening on ', port))
