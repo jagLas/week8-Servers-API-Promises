8b6662a00008a4e3e04934c8b0657c3a12f83b8e
jagLas <jglasher@gmail.com>
2023-02-17 12:33:35 -0500

    practice done

--- practice-for-week-08-serve-static-assets/server.js
+++ practice-for-week-08-serve-static-assets/server.js
@@ -4,11 +4,24 @@ const fs = require("fs");
 const server = http.createServer((req, res) => {
   // Your code here
   console.log(req.method, req.url);
-  if (req.method === 'GET' && req.url === '/') {
+
+  if (req.method === 'GET' && req.url.startsWith('/static')) {
+    let url = req.url.split('/');
+    url = './assets/' + url.slice(2).join('/');
+    console.log(url)
+    const extension = req.url.split('.')[1];
+    const file = fs.readFileSync(url);
+
+    res.statusCode = 200;
+    res.setHeader('content-type', `text/${extension}`);
+    return res.end(file);
+  }
+
+  if (req.method === 'GET') {
     const html = fs.readFileSync('./index.html', 'utf-8');
     res.setHeader('content-type', 'text/html')
     res.statusCode = 200;
-    res.end(html);
+    return res.end(html);
   }
 });
 
