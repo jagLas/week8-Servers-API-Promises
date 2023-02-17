50d6be0e46b5381d18d2e8d4879d70754b0c2efc
jagLas <jglasher@gmail.com>
2023-02-17 12:18:41 -0500

    phase 1 done

--- practice-for-week-08-serve-static-assets/server.js
+++ practice-for-week-08-serve-static-assets/server.js
@@ -3,6 +3,13 @@ const fs = require("fs");
 
 const server = http.createServer((req, res) => {
   // Your code here
+  console.log(req.method, req.url);
+  if (req.method === 'GET' && req.url === '/') {
+    const html = fs.readFileSync('./index.html', 'utf-8');
+    res.setHeader('content-type', 'text/html')
+    res.statusCode = 200;
+    res.end(html);
+  }
 });
 
 const port = 5000;
