9d466acbfd3d134d99ec78a751f73aa6accce5a2
jagLas <jglasher@gmail.com>
2023-02-24 12:35:15 -0500

    done phase 2 and 3

--- practice-for-week-08-adventure-game-navigation-long-practice/server.js
+++ practice-for-week-08-adventure-game-navigation-long-practice/server.js
@@ -16,6 +16,7 @@ const server = http.createServer((req, res) => {
   let reqBody = '';
   req.on('data', (data) => {
     reqBody += data;
+
   });
 
   req.on('end', () => { // After the assembly of the request body is finished
@@ -44,12 +45,50 @@ const server = http.createServer((req, res) => {
     }
 
     // Phase 2: POST /player
-    if (req.method === 'POST' && req.url === '/player') {
-      
+    if (req.method === 'POST' && req.url.startsWith('/player') === true) {
+      res.statusCode = 302;
+
+      // console.log(req.body)
+      let startingRoom = world.rooms[req.body.roomId];
+      player = new Player (req.body.name, startingRoom);
+
+      res.setHeader('location', `/rooms/${startingRoom.id}`);
+      console.log(`adding ${player}`);
+      return res.end();
+    }
+
+    if (player === undefined) {
+      res.statusCode = 302;
+      res.setHeader('location', '/');
+      return res.end();
     }
 
     // Phase 3: GET /rooms/:roomId
+    if (req.method === 'GET' && req.url.startsWith('/rooms')) {
 
+      //checks if requested room is same as players current room
+      const roomId = req.url.split('/')[2];
+      if (roomId != player.currentRoom.id) {
+        //redirects to the correct room if not
+        res.statusCode = 302;
+        res.setHeader('location', `/rooms/${player.currentRoom.id}`)
+        return res.end();
+      }
+
+      //checks if url is requesting a room
+      if (req.url.split('/').length === 3) {
+        res.statusCode = 200;
+        res.setHeader('content-type', 'text/html');
+        const htmlString = fs.readFileSync('./views/room.html', 'utf-8');
+        const room = player.currentRoom;
+        const htmlPage = htmlString
+          .replace(/#{roomName}/g, room.name)
+          .replace(/#{inventory}/g, player.inventoryToString())
+          .replace(/#{roomItems}/g, room.itemsToString())
+          .replace(/#{exits}/g, room.exitsToString());
+        return res.end(htmlPage);
+      }
+    }
     // Phase 4: GET /rooms/:roomId/:direction
 
     // Phase 5: POST /items/:itemId/:action
