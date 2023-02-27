bceb31e3dc6dc62d24b17afa6d050ad0f76e3d19
jagLas <jglasher@gmail.com>
2023-02-27 13:25:56 -0500

    phase 2 done

--- practice-for-week-08-create-and-handle-promises/phase-2.js
+++ practice-for-week-08-create-and-handle-promises/phase-2.js
@@ -1,24 +1,65 @@
 function stretch(timeLeft) {
   // refactor your code from phase 1
   // Your code here
+  const stretchTime = 1000;
+
+  return new Promise ((resolve, reject) => {
+    if (stretchTime <= timeLeft) {
+      console.log('done stretching');
+      resolve(timeLeft - stretchTime);
+    } else {
+      throw new Error('Not enough time to stretch')
+    }
+  })
 }
 
 
 function runOnTreadmill(timeLeft) {
   // refactor your code from phase 1
   // Your code here
+  const treadmillTime = 500;
+
+  if (treadmillTime <= timeLeft) {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        console.log('done running on the treadmill')
+        resolve(timeLeft - treadmillTime)
+      }, treadmillTime)
+    })
+  } else {
+    throw new Error('Not enough time to run on the treadmill')
+  }
 }
 
 
 function liftWeights(timeLeft) {
   // refactor your code from phase 1
   // Your code here
+  const liftingTime = 2000;
+
+  if (liftingTime <= timeLeft) {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        console.log('done lifting weights')
+        resolve(timeLeft - liftingTime)
+      }, liftingTime)
+    })
+  } else {
+    throw new Error('Not enough time to lift weights')
+  }
 }
 
 
 function workout(totalTime) {
   // refactor your code from phase 1
   // Your code here
+  stretch(totalTime)
+  .then((timeLeft) => runOnTreadmill(timeLeft))
+  .then(timeLeft => liftWeights(timeLeft))
+  .then((timeLeft) => console.log(`done working out with ${timeLeft / 1000} seconds left`))
+  .catch(error => {
+    console.error('Error:', error.message)
+  });
 }
 
 /* ============================ TEST YOUR CODE ============================
@@ -28,7 +69,7 @@ Comment in each invocation of your workout function below and run the file
 */
 
 
-// workout(500);
+workout(500);
   // should print out the following:
     // Error:  you dont have enough time to stretch
 
