c55d88633b2a049d3b80b8e46f92df60e921ba15
jagLas <jglasher@gmail.com>
2023-02-27 13:39:20 -0500

    practice done

--- practice-for-week-08-promise-all/phase-1.js
+++ practice-for-week-08-promise-all/phase-1.js
@@ -27,10 +27,8 @@ function liftWeights() {
 
 function workout() {
   // refactor this code to use Promise.all
-  stretch()
-    .then(runOnTreadmill)
-    .then(liftWeights)
-    .then(() => console.log("done working out"))
+  Promise.all([stretch(), runOnTreadmill(), liftWeights()])
+    .then(() => console.log('done working out'))
     .catch((err) => console.log(err));
 }
 
