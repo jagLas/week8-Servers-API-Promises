3dfcd3151798edcef5332eae3434f8a32f9c2726
jagLas <jglasher@gmail.com>
2023-03-01 14:03:05 -0500

    phase 3 done

--- practice-for-week-08-fetch-from-server/assets/js/your-code.js
+++ practice-for-week-08-fetch-from-server/assets/js/your-code.js
@@ -5,10 +5,17 @@ export function getAllDogs() {
 
 export function getDogNumberTwo() {
     // Your code here
+    return fetch('/dogs/2')
 }
 
 export function postNewDog() {
     // Your code here
+    const textBody = new URLSearchParams([['name', 'telemcus'], ['age', 4]])
+    return fetch('/dogs', {
+        'headers': {'Content-type': 'application/x-www-form-urlencoded'},
+        'method': 'POST',
+        'body': textBody
+    })
 }
 
 export function postNewDogV2(name, age) {
